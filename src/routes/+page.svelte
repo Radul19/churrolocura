0fb7e608374cc593fda3ed679e04f79de2499b22
Radul19 <radulito19@gmail.com>
2023-06-22 11:13:48 -0400

    clean

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,48 @@
 <script>
-  import { onMount } from "svelte";
-  import loading from '../public/loading.gif'
-
-  //   import { browser } from "$app/environment";
-  let iw;
-  let ih;
-  onMount(() => {
-    let desktop = iw > ih && true;
-
-    if (desktop) {
-        console.log('waaaaat')
+    export const prerender = true;
+    import { onMount } from "svelte";
+    import loading from "../public/loading.gif";
+  
+      import { browser } from "$app/environment";
+    let iw;
+    let ih;
+    onMount(() => {
+      let desktop = iw > ih && true;
+  
+      if (desktop) {
         window.location.href = "/desktop/home";
-    } else {
-        console.log('waaaaat2')
-      window.location.href = "/mobile/home";
-    }
-  });
-</script>
-
-<svelte:window bind:innerWidth={iw} bind:innerHeight={ih} />
-<div class="">
+      } else {
+        window.location.href = "/mobile/home";
+      }
+    });
+  </script>
+  
+  <!-- <svelte:window bind:innerWidth={iw} bind:innerHeight={ih} /> -->
+  <div class="">
+      <p>Visit <a href="/mobile/home">Mobile folder</a> to read the documentation</p>
     <!-- <p>Loading...</p> -->
-    <img src={loading} alt="laoding">
-</div>
-<!-- <p>Visit <a href="/mobile/home">Mobile folder</a> to read the documentation</p> -->
-
-<style>
-    div{
-        height: 100vh;
-        width: 100vw;
-        box-sizing: border-box;
-        background-color: #fff;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+    <!-- <img src={loading} alt="laoding" /> -->
+  </div>
+  
+  
+  <style>
+    div {
+      height: 100vh;
+      width: 100vw;
+      box-sizing: border-box;
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
     /* p{
-        margin: 0px;
-        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        text-align: center;
-    } */
-    img{
-        /* width: 100%; */
-        max-width: 320px;
+          margin: 0px;
+          font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+          text-align: center;
+      } */
+    img {
+      /* width: 100%; */
+      max-width: 320px;
     }
-</style>
+  </style>
+  
